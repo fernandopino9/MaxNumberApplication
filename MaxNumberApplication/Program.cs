0f0330047e645eb6ee36bb0e92dad5310ee764a3
34608 <34608@DESKTOP-IS775AF>
2021-11-19 19:44:58 +0200

    Agregar archivos de proyecto.

---
+++ MaxNumberApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaxNumberApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the first number");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the third number");
+            double num3 = double.Parse(Console.ReadLine());
+
+           
+            Console.WriteLine(Math.Max(Math.Max(num1, num2), num3));
+
+
+        }
+    }
+}
